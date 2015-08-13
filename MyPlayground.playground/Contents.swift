8f6d8ce83472dddda8b8ea4d61937d4617e9f2e6
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-08-12 20:17:01 -0400

    Started Playground Exercise

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,139 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+var name:String = "Rosario"
+
+name = "Lindsay"
+
+var unknownString:String
+
+unknownString = "3"
+
+var int = 4
+
+3 * int;
+
+var a:Int = 5
+
+var b = a * 3
+
+var c:Double = 5 / 2
+
+c * 2
+
+c * 4
+
+c * Double(int)
+
+var longSentence = str + " " + name
+
+var newString = "My son is \(int)!"
+
+var a = 3.5
+
+var b = 4
+
+var c = a * Double(b)
+
+var str = "\(a) multiplied by \(b) equals \(c)"
+
+
+
+var arr = [1, 2, 3, 4, 5]
+
+arr [3]
+
+arr.append(6)
+
+println(arr)
+
+arr.removeAtIndex(2)
+
+println(arr)
+
+arr.removeLast()
+
+println(arr)
+
+arr.removeRange(1...2)
+
+println(arr)
+
+var newArr = [1, 2, 4.7, "Declan"]
+
+var emptyArr:[Int]
+
+var dict = ["name": "Rosario", "age": 35, "gender": "male"]
+
+println(dict["name"]!)
+
+dict["haircolor"] = "Brown"
+
+dict["age"] = "old"
+
+println(dict)
+
+dict["age"] = nil
+
+println(dict)
+
+var name = dict["name"]
+
+var myString = "My name is \(name!)";
+
+*/
+
+var arr = [2, 4, 6, 8,]
+
+arr.removeAtIndex(0)
+
+arr.append(10)
+
+var dict = ["name": "Rosario", "age": 35]
+
+var name = dict["name"]!
+
+var age = dict["age"]!
+
+var aboutMe = "My name is \(name) and my age is \(age)."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
